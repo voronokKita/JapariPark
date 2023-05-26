bd8b6d8011cdcf6ff86d98447c489fe84970cf5a
voronokKita <kitavoronok@gmail.com>
2023-05-26 11:08:41 +0300

    Basic compose
    Committed on branch 'compose_frontend'

--- FriendsFrontend/manage.py
+++ FriendsFrontend/manage.py
@@ -1,5 +1,10 @@
 """Manager of the Japari Park: Friends frontend."""
 
+__author__ = 'Voronok Kita'
+__copyright__ = 'Copyright (C) 2023 Voronok Kita'
+__license__ = 'TODO'
+__version__ = 'dev'
+
 import friends
 
 
